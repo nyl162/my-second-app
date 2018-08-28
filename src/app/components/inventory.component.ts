d142255e9748978e806f0818c9e8af6bbb0aae84
NGYL <nyl162@hotmail.com>
2018-08-28 16:18:06 +0800

    Step 2

--- src/app/components/inventory.component.ts
+++ src/app/components/inventory.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface LineItem{
+  label: string;
+  image: string;
+  quantity: number;
+}
+
 @Component({
   selector: 'app-inventory',
   templateUrl: './inventory.component.html',
@@ -7,9 +13,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InventoryComponent implements OnInit {
 
+  inventory: LineItem[] = [
+    {label: "Acron Squash" , image: "acorn_squash.png", quantity: 10},
+    {label: "Apple" , image: "apple.png", quantity: 2},
+    {label: "BlurBerries" , image: "blueberries.png", quantity: 20},
+    {label: "Carrot" , image: "carrot.png", quantity: 108},
+    {label: "Hei! Harold" , image: "harold.png", quantity: 1},
+    {label: "Coconut" , image: "tomato.png", quantity: 1000}
+  ];
+
   constructor() { }
 
   ngOnInit() {
   }
-
+  AddItem(selectIndex : number){
+    console.log("Item : " + this.inventory[selectIndex].label)
+  }
 }
